ad0a65ea3068febfc361a13b0aca9116ed196c1f
Soumya Rayast <113257526+soumya-rayast@users.noreply.github.com>
2024-04-10 12:44:00 +0530

    Navbar

--- clients/src/components/Navbar.jsx
+++ clients/src/components/Navbar.jsx
@@ -2,8 +2,13 @@ import React from 'react'
 import { IconButton } from "@mui/material"
 import { Search, Person, Menu } from "@mui/icons-material"
 import variables from "../Styles/variables.scss"
+import { useState } from 'react'
+import { useSelector, useDispatch } from "react-redux"
+import "../Styles/Navbar.scss"
+import { Link, useNavigate } from "react-router-dom"
+import { setLogout } from "../Redux/State"
 
-export const Navbar = () => {
+export const Navbar = (user) => {
     return (
         <div className='navbar'>
             <a href="/">
@@ -19,10 +24,22 @@ export const Navbar = () => {
                 {user ? <a href='/create-listening'>Become a Host</a> : <a href='/login'>Become a Host</a>}
                 <button className='navbar_right_account'>
                     <Menu style={{ color: variables.darkgrey }} />
-                    {}
-                    <Person style={{ color: variables.darkgrey }} />
+                    {!user ? (
+                        <Person style={{ color: variables.darkgrey }} />
+                    ) : (
+                        <img
+                        src={`http://localhost:3001/${user.profileImagePath.replace(
+                          "public",
+                          ""
+                        )}`}
+                        alt="profile photo"
+                        style={{ objectFit: "cover", borderRadius: "50%" }}
+                      />
+                )}
                 </button>
             </div>
         </div>
+        // <>
+        // homepage</>
     )
 }
