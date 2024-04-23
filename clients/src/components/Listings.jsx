1e09b8ddbb6715820a398961432aaccaf14325de
Soumya Rayast <113257526+soumya-rayast@users.noreply.github.com>
2024-04-24 00:59:42 +0530

    Working on ListingCard

--- clients/src/components/Listings.jsx
+++ clients/src/components/Listings.jsx
@@ -1,19 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { categories } from '../data'
 import "../Styles/Listings.scss"
-import ListingCarD from './ListingCard'
+import ListingCard from './ListingCard'
 import Loader from "./Loader"
+import { useDispatch, useSelector } from "react-redux"
+import setListings from "../Redux/State"
 
 const Listings = () => {
-  
+  const dispatch = useDispatch();
+  const [loading, setLoading] = useState(true);
+  const [selectedCategory, setSelectedCategory] = useState("All");
+  const listings = useSelector((state) => state.listings);
+
+  const getFeedListings = async () => {
+    try {
+      const response = await fetch(
+        selectedCategory !== "All" 
+        ? `http://localhost3000/properties?category=${selectedCategory}`
+        :"http://localhost3000/properties",
+        { method: "GET", });
+
+        const data = await response.json();
+        dispatch(setListings({listings:data}));
+        setLoading(false);
+
+    } catch (err) {
+      console.log("Fetch Listings Failed",err.message)
+    }
+  };console.log(listings)
+
+  useEffect(()=>{
+    getFeedListings();
+  },[selectedCategory]);
+
   return (
     <div className='category-list'>
-      {categories?.map((category,index) => (
-        <div className='category' key={index}>
-           <div className="category_icon">
-           {category.icon}
-           </div>
-           <p>{category.label}</p>
+      {categories?.map((category, index) => (
+        <div className='category' key={index} onClick={() => setSelectedCategory(category.label)}>
+          <div className="category_icon">
+            {category.icon}
+          </div>
+          <p>{category.label}</p>
         </div>
       ))}
     </div>
