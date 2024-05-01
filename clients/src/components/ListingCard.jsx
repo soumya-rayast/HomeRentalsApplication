629e703af4e386767b03bb42893daf217caf98a0
Soumya Rayast <113257526+soumya-rayast@users.noreply.github.com>
2024-05-01 23:36:42 +0530

    Working

--- clients/src/components/ListingCard.jsx
+++ clients/src/components/ListingCard.jsx
@@ -1,5 +1,10 @@
 import React from 'react'
 import "../Styles/ListingCard.scss"
+import {
+  ArrowForwardIos,
+  ArrowBackIosNew,
+  Favorite,
+} from "@mui/icons-material";
 const ListingCard = ({ listingId,
   creator,
   listingPhotoPaths,
@@ -14,9 +19,12 @@ const ListingCard = ({ listingId,
       <div className='listing-card'>
         <div className="slider-container">
           <div className='slider'>
-            {listingPhotoPaths?.map((photo,index)=>(
-              <div>
-                
+            {listingPhotoPaths?.map((photo, index) => (
+              <div key={index} className='slide'>
+                <img src={`http://localhost:3001${photo.replace("public", "")}`} alt={`photo ${index + 1}`} />
+                <div className="prev-button">
+                  <ArrowBackIosNew/>jj
+                </div>
               </div>
             ))}
           </div>
